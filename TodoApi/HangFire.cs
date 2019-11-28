5bf16204075dccc34a6444ef9557639788f45e76
Ogba Anthony <ogbaanthony6@gmail.com>
2019-11-28 14:43:20 +0100

    Modified Hangfire

--- TodoApi/HangFire.cs
+++ TodoApi/HangFire.cs
@@ -29,24 +29,29 @@ namespace TodoApi
         public void SkippedDailyTask()
         {
             var pendingtasks = GetPendingTasks();
-            if (pendingtasks != null)
+            if (pendingtasks.Count() == 0)
             {
-                foreach (var item in pendingtasks)
-                {
-                    if (item.EndTime.Minute.Equals(DateTime.Now.Minute))
-                    {
-                        item.StatusReturner = StatusReturner.Skipped;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Empty List");
-                    }
-                }
-                context.SaveChanges();
+                Console.WriteLine("Empty List");
             }
             else
             {
-                Console.WriteLine("Empty List");
+                foreach (var item in pendingtasks)
+                {
+                    if (item.EndTime.Hour <= DateTime.Now.Hour)
+                    {
+                        if (item.EndTime.Minute <= DateTime.Now.Minute)
+                        {
+                            item.StatusReturner = StatusReturner.Skipped;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Empty List");
+                        }
+                        context.SaveChanges();
+                    }
+
+                }
+
             }
         }
 
@@ -54,25 +59,29 @@ namespace TodoApi
         public void CompletedDailyTask()
         {
             var completedtasks = GetOngoingTasks();
-            if (completedtasks != null)
+            if (completedtasks.Count() == 0)
             {
-                foreach (var item in completedtasks)
-                {
-                    //var beforetime = item.StartTime.Minute - 5;
-                    if (item.EndTime.Minute.Equals(DateTime.Now.Minute))
-                    {
-                        item.StatusReturner = StatusReturner.Completed;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Empty List");
-                    }
-                }
-                context.SaveChanges();
+                Console.WriteLine("Empty List");
             }
             else
             {
-                Console.WriteLine("Empty List");
+                foreach (var item in completedtasks)
+                {
+                    if (item.EndTime.Hour <= DateTime.Now.Hour)
+                    {
+                        if (item.EndTime.Minute <= DateTime.Now.Minute)
+                        {
+                            item.StatusReturner = StatusReturner.Completed;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Empty List");
+                        }
+                        context.SaveChanges();
+                    }
+
+                }
+
             }
 
 
@@ -80,27 +89,31 @@ namespace TodoApi
 
         public void PendingDailyTask()
         {
-           
+
             var scheduledtasks = GetScheduledTasks();
-            if (scheduledtasks != null)
+            if (scheduledtasks.Count() == 0)
             {
-                foreach (var item in scheduledtasks)
-                {
-                    //var beforetime = item.StartTime.Minute - 5;
-                    if (item.StartTime.Minute.Equals(DateTime.Now.Minute))
-                    {
-                        item.StatusReturner = StatusReturner.Pending;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Empty List");
-                    }
-                }
-                context.SaveChanges();
+                Console.WriteLine("Empty List");
             }
             else
             {
-                Console.WriteLine("Empty List");
+                foreach (var item in scheduledtasks)
+                {
+
+                    if (item.StartTime.Hour <= DateTime.Now.Hour)
+                    {
+                        if (item.StartTime.Minute <= DateTime.Now.Minute)
+                        {
+                            item.StatusReturner = StatusReturner.Pending;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Empty List");
+                        }
+                        context.SaveChanges();
+                    }
+                }
+
             }
         }
 
@@ -108,60 +121,59 @@ namespace TodoApi
         {
 
             var unscheduledTask = GetUnscheduledTasks();
-            if (unscheduledTask != null)
-            {
-                    foreach (var item in unscheduledTask)
-                    {
-                        if (item.StartTime.Day.Equals(DateTime.Now.Day))
-                        {
-                            item.StatusReturner = StatusReturner.Scheduled;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Empty List");
-                        }
-                   
-                    }
-                    context.SaveChanges();
-            }
-            else
+            if (unscheduledTask.Count() == 0)
             {
                 Console.WriteLine("Empty List");
             }
+            else
+            {
+                foreach (var item in unscheduledTask)
+                {
+                    if (item.StartTime.Day <= DateTime.Now.Day)
+                    {
+                        item.StatusReturner = StatusReturner.Scheduled;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Empty List");
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
 
-       
+
 
         public IEnumerable<TodoItem> GetUnscheduledTasks()
         {
-            var unscheduleditems = context.TodoItems.Where(x => x.StatusReturner == StatusReturner.Unscheduled);
+            var unscheduleditems = context.TodoItems.Where(x => x.StatusReturner == StatusReturner.Unscheduled).ToList();
             return unscheduleditems;
         }
         public IEnumerable<TodoItem> GetScheduledTasks()
         {
-            var scheduledItem = context.TodoItems.Where(x => x.StatusReturner == StatusReturner.Scheduled);
+            var scheduledItem = context.TodoItems.Where(x => x.StatusReturner == StatusReturner.Scheduled).ToList();
             return scheduledItem;
 
 
         }
         public IEnumerable<TodoItem> GetPendingTasks()
         {
-            var pendingtasks = context.TodoItems.Where(x => x.StatusReturner == StatusReturner.Pending);
+            var pendingtasks = context.TodoItems.Where(x => x.StatusReturner == StatusReturner.Pending).ToList();
             return pendingtasks;
         }
         public IEnumerable<TodoItem> GetOngoingTasks()
         {
-            var ongoingItem = context.TodoItems.Where(x => x.StatusReturner == StatusReturner.Ongoing);
+            var ongoingItem = context.TodoItems.Where(x => x.StatusReturner == StatusReturner.Ongoing).ToList();
             return ongoingItem;
         }
     }
 
 
 
-   // public class HangFire
-   // {
-   //
-   // }
+    // public class HangFire
+    // {
+    //
+    // }
 
 
 
